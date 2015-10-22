c5163b069266998127f40f13fa559d43cb674996
Roshan Patel <roshanpatel06@gmail.com>
2015-10-22 11:27:46 -0400

    Recursivly navigation through an input directory.  Test successful with /testing

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # indexer
 
 Work in progress.
+
+
+#Current use:
+>$ make
+>$ ./index outputfiles_doesntworkyet testing
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include "tokenizer.h"
-
 
 /* 
 	Agument validity checking.  Includes -help option.
 	Returns error message on invalid input.
 */
-void argCheck(int argc, char** argv){
+void argCheck(int argc, char** argv)
+{
 
 	char* argNumErr = "Incorrect number of arguments given.  Use \"index -help\" for proper usage infomation.\n";  
 	switch(argc){
@@ -25,8 +27,13 @@ void argCheck(int argc, char** argv){
 	}
 }
 
-
-int parseFile(FILE* file){
+/* 
+	Creates and parses through an input file using a tokenizer object.
+	Caller is responsible for opening and closing the input file.
+	Tokenizer object is intialized and destroyed by this function.
+*/
+int parseFile(FILE* file)
+{
 
 	TokenizerT* tk 	= TKCreate(file);
 
@@ -37,6 +44,76 @@ int parseFile(FILE* file){
 	}
 
 	TKDestroy(tk);
+	return 0;
+}
+
+/*
+	exploreDirectories travels through directories given a char* inputname.
+	Recursive calls are initiated on file pointers until Files are reached.
+	The base case handles files by calling parseFile.
+*/
+int exploreDirectories(char* inputname)
+{
+
+	/* enum value will keep record of current type */
+	enum{File, Directory} type 		= Directory;
+	
+	void *fileptr					= opendir(inputname);
+	struct dirent *entry;
+
+	if(fileptr == NULL){
+		type 		= File;
+		fileptr 	= fopen(inputname, "r");
+		
+		if(fileptr == NULL){
+			perror(fileptr);
+			return 0;
+		}
+	}
+
+	/* Current recursive iteration is a directory */
+	if(type == Directory){
+
+		DIR *directory 	= (DIR *)fileptr;
+		for(;;){
+			entry 	= readdir(directory);
+
+			/* Empty directory */
+			if(entry == NULL){
+				break;
+			}
+
+			/* Protection against recursing backwards */
+			if(!strcmp(entry->d_name,".") || !strcmp(entry->d_name,"..")){
+				continue;
+			}
+
+			/* Save filepath as string */
+			char* next 	= (char *)malloc(strlen(inputname) + strlen(entry->d_name) + 2);
+			strcpy(next, inputname);
+			strcat(next, "/");
+			strcat(next, entry->d_name);
+
+			/* Recursive call */
+			printf("%s\n",next);
+
+			exploreDirectories(next);
+
+			//printf("\t%s\n", entry->d_name);
+
+			free(next);
+		}
+		closedir(directory);
+
+	}
+	/* Current recursive iteration is a file (Base case) */
+	else{
+		printf(">>Exploring file: %s \n", inputname);
+		FILE *file = (FILE *)fileptr;
+		parseFile(file);
+		printf("\n\n");
+		fclose(file);
+	}
 
 	return 0;
 }
@@ -47,13 +124,13 @@ int parseFile(FILE* file){
 	SHOULD SPIT OUT INPUT FILE'S ALPHANUM CHARS IN LOWERCASE 
 	Usage:  ./index whatever gastrobot.txt
 */
-int main(int argc, char **argv){
-
+int main(int argc, char **argv)
+{
 	argCheck(argc, argv);
+	exploreDirectories(argv[2]);
 
-	FILE *input_file 		= fopen(argv[2],"r");
-	
-	parseFile(input_file);
+	// FILE *input_file 		= fopen(argv[2],"r");
+	// parseFile(input_file);
 
 
 	return 0;
